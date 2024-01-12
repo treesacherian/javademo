804a6f45b0898dd2335c9cefc46829ee063b2d35
treesacherian <treesa.cherian@gmail.com>
2024-01-12 12:06:13 +0000

    datatypes demo

---
+++ src/datatypes/DatypeDemo.java
@@ -0,0 +1,48 @@
+package datatypes;
+
+public class DatypeDemo {
+
+	public static void main(String[] args) {
+		
+		byte b = 8;
+		short sh = 300;
+		int in =99999;
+		long ln= 8765437869000L;
+		float fl=87.98F;
+		double db=765432.87;
+		char ch='y';
+		String name = "Treesa";
+		boolean check=false;
+		System.out.println("byte");
+		System.out.println(b);
+		System.out.println("short");
+		System.out.println(sh);
+		System.out.println("int");
+		System.out.println(in);
+		System.out.println("long");
+		System.out.println(ln);
+		System.out.println("float");
+		System.out.println(fl);
+		System.out.println("double");
+		System.out.println(db);
+		System.out.println("char");
+		System.out.println(ch);
+		System.out.println("String");
+		System.out.println(name);
+		System.out.println("boolean");
+		System.out.println(check);
+		
+		String fname="Treesa ";
+		String lname = "Cherian";
+		String fullname = fname+lname;
+		System.out.println(fname);
+		System.out.println(lname);
+		System.out.println(fullname);
+		char ch1='y';
+		char ch2='n';
+		System.out.println(ch1-ch2);
+		
+		
+	}
+	
+}
