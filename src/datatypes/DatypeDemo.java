d718178b28346c830ffc8f9a880372a18bf82aaf
treesacherian <treesa.cherian@gmail.com>
2024-01-12 12:14:56 +0000

    datatypes demo amended

--- src/datatypes/DatypeDemo.java
+++ src/datatypes/DatypeDemo.java
@@ -9,29 +9,28 @@ public class DatypeDemo {
 		int in =99999;
 		long ln= 8765437869000L;
 		float fl=87.98F;
-		double db=765432.87;
+		double db=765432.87;;
 		char ch='y';
 		String name = "Treesa";
 		boolean check=false;
-		System.out.println("byte");
+		System.out.print("byte:");
 		System.out.println(b);
-		System.out.println("short");
+		System.out.print("short:");
 		System.out.println(sh);
-		System.out.println("int");
+		System.out.print("int:");
 		System.out.println(in);
-		System.out.println("long");
+		System.out.print("long:");
 		System.out.println(ln);
-		System.out.println("float");
+		System.out.print("float:");
 		System.out.println(fl);
-		System.out.println("double");
+		System.out.print("double:");
 		System.out.println(db);
-		System.out.println("char");
+		System.out.print("char:");
 		System.out.println(ch);
-		System.out.println("String");
+		System.out.print("String:");
 		System.out.println(name);
-		System.out.println("boolean");
+		System.out.print("boolean:");
 		System.out.println(check);
-		
 		String fname="Treesa ";
 		String lname = "Cherian";
 		String fullname = fname+lname;
@@ -41,6 +40,8 @@ public class DatypeDemo {
 		char ch1='y';
 		char ch2='n';
 		System.out.println(ch1-ch2);
+		String name1 = null;
+		System.out.println(name1);
 		
 		
 	}
