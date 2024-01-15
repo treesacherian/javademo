e65faebdeb5feef8002ca52fe7fce41ad265f0fc
treesacherian <treesa.cherian@gmail.com>
2024-01-15 15:42:44 +0000

    Class files changed

--- src/classDemo/App.java
+++ src/classDemo/App.java
@@ -1,30 +1,30 @@
 package classDemo;
 
 public class App {
-	
+
 	public static void main(String[] args) {
-	
-		Person  family1=new Person();
-		Person  family2 =new Person();
-		Person  family3 =new Person();
-		
-		family1.name="Jacob";
+
+		Person family1 = new Person();
+		Person family2 = new Person();
+		Person family3 = new Person();
+
+		family1.name = "Jacob";
 		family1.occupation = "employed ";
 		family1.age = 55;
 		family1.nationality = "British";
-		
-		family2.name="Samuel";
+
+		family2.name = "Samuel";
 		family2.occupation = "Student ";
 		family2.age = 22;
 		family2.nationality = "British ";
-		
-		family3.name="Joshua";
+
+		family3.name = "Joshua";
 		family3.occupation = "Student ";
-		family3.age =17 ;
+		family3.age = 17;
 		family3.nationality = "British";
-		
-		Person[] familyMembers= {family1, family2, family3};
-		for(Person member : familyMembers) {
+
+		Person[] familyMembers = { family1, family2, family3 };
+		for (Person member : familyMembers) {
 			member.displayDetails();
 		}
 	}
--- src/classDemo/Person.java
+++ src/classDemo/Person.java
@@ -1,16 +1,16 @@
 package classDemo;
 
 public class Person {
- String name;
- String occupation;
- int age;
- String nationality;
- 
- void displayDetails() {
-	 System.out.println("Name: " +name);
-	 System.out.println("Occupation: "+occupation);
-	 System.out.println("AGE: "+age);
-	 System.out.println("Nationality: "+nationality);
-	 System.out.println("_____________");
- }
+	String name;
+	String occupation;
+	int age;
+	String nationality;
+
+	void displayDetails() {
+		System.out.println("Name: " + name);
+		System.out.println("Occupation: " + occupation);
+		System.out.println("AGE: " + age);
+		System.out.println("Nationality: " + nationality);
+		System.out.println("_____________");
+	}
 }
